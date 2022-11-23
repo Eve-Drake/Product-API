34b62c6c3a72bcda6fca7db25fd48362fb9b24d4
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-23 14:27:11 +0000

    Adding Products (Simulated)

--- src/AddProduct.tsx
+++ src/AddProduct.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { printSourceLocation } from 'graphql'
+import React, { useState } from 'react'
 
 const AddProduct = () => {
-
+    const [title, setTitle] = useState<string>('')
+    const [price, setPrice] = useState<number>(0)
+    const [description, setDescription] = useState<string>('')
+    
+    
     const handleSubmit = () =>{
         
     }
   return (
     <form onSubmit={handleSubmit()}>
+        <input value={title} onChange={(e)=>setDescription(e.target.value)}/>
+        <input />
         <input />
         <button type='submit'>Submit</button>
     </form>
