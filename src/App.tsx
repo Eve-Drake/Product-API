e8ab100cca2988d8341c9daad4efa1b7f015671b
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-25 11:01:23 +0000

    Removed some boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import AddProduct from './componenets/AddProduct'
-import reactLogo from './assets/react.svg'
 import GetProductData from './componenets/GetProductData'
 import UpdateData from './UpdateData'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <AddProduct />
