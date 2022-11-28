e63ceea651042ffc57782701511a9d9d9a816503
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-28 15:10:09 +0000

    Delete

--- src/components/DeleteProduct.tsx
+++ src/components/DeleteProduct.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 const DeleteProduct = () => {
+
+  //temp id
+  const id = 4
+
+  const handleDelete = () =>{
+    fetch(`https://dummyjson.com/products/${id}`, {
+      method: 'DELETE'
+    })
+    .then((result) => result.json())
+    .then(console.log)
+  }
   return (
     <div>DeleteProduct</div>
   )
