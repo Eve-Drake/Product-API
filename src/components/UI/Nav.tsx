6a703711be0fbcddd4f545f70cb81c35aa508c41
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-12-14 14:21:59 +0000

    Adding Nav list

--- src/components/UI/Nav.tsx
+++ src/components/UI/Nav.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Nav = () => {
   return (
     <>
-    
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </>
   )
 }
