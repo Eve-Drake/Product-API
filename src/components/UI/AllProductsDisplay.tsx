e56ded457e28bf1ae7959c879d04d0203f564544
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-01-24 14:00:50 +0000

    Changing to index for map

--- src/components/UI/AllProductsDisplay.tsx
+++ src/components/UI/AllProductsDisplay.tsx
@@ -12,8 +12,8 @@ const AllProductsDisplay = ({products, activeProductCategory}: Props) => {
     <>
         <h2>{activeProductCategory}</h2>
         <div className='products-display-grid'>
-            {products.map(product =>(
-            <div key={product.id}>
+            {products.map( (product, index) =>(
+            <div key={index}>
                 <DisplayData product={product} />
             </div>
             ))}
